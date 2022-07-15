e86577ad6a0ca812ece42c8659ac926062f0274b
Kirbman10 <38724165+Kirbman10@users.noreply.github.com>
2022-07-16 00:20:40 +0200

    animaninmaninaimatro is good

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,9 +7,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField, Range(0f, 100f)] public float _moveSpeed = 5f;
     public Rigidbody2D _body;
     Vector2 _movement;
+    public Animator _animator;
 
     public Direction CurrentFacingDirection { get; set; } = Direction.Down;
 
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -17,6 +22,31 @@ public class PlayerMovement : MonoBehaviour
         _movement.x = Input.GetAxisRaw("Horizontal");
         _movement.y = Input.GetAxisRaw("Vertical");
 
+        if (_movement.x > 0) CurrentFacingDirection = Direction.Right;
+        else if (_movement.x < 0) CurrentFacingDirection = Direction.Left;
+        else if (_movement.y > 0) CurrentFacingDirection = Direction.Up;
+        else if (_movement.y < 0) CurrentFacingDirection = Direction.Down;
+
+        _animator.SetBool("up", false);
+        _animator.SetBool("down", false);
+        _animator.SetBool("left", false);
+        _animator.SetBool("right", false);
+
+        switch (CurrentFacingDirection)
+        {
+            case Direction.Up:
+                _animator.SetBool("up", true);
+                break;
+            case Direction.Down:
+                _animator.SetBool("down", true);
+                break;
+            case Direction.Left:
+                _animator.SetBool("left", true);
+                break;
+            case Direction.Right:
+                _animator.SetBool("right", true);
+                break;
+        }
     }
 
     private void FixedUpdate()
