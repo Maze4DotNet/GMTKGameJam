8a9faaf61b59a6817ae29d715364a795f5748f6f
Maze4DotNet <m.verheije@4dotnet.nl>
2022-07-16 00:19:42 +0200

    ik ga nu naar de ikea om te pooooteav

---
+++ Assets/Scripts/Director/GlobalParameters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GlobalParameters : MonoBehaviour
+{
+    [SerializeField, Range(0f, 10f)] public float _requiredPushingTime;
+
+}
