6d6fc2a7c2e4dbd2215bcd1292e9ada79e9493d3
Maze4DotNet <m.verheije@4dotnet.nl>
2022-07-16 13:03:41 +0200

    pushingnnn

---
+++ Assets/Scripts/Misc/DirVec.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+internal static class DirVec
+{
+    public static Vector2 GetVector(Direction direction)
+    {
+        switch (direction)
+        {
+            case (Direction.Up):
+            {
+                return new Vector2(0f, 1);
+            }
+            case (Direction.Down):
+            {
+                return new Vector2(0f, -1);
+            }
+            case (Direction.Left):
+            {
+                return new Vector2(-1,0);
+            }
+            default:
+            {
+                return new Vector2(1,0);
+            }
+        }
+    }
+}
--- Assets/Scripts/Object/PushDetection.cs
+++ Assets/Scripts/Object/PushDetection.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PushDetection : MonoBehaviour
 {
     public GameObject _director;
-    public GlobalParameters _globalParameters;
+    private GlobalParameters _globalParameters;
 
     public PushableObjectHitboxScript _topHitbox;
     public PushableObjectHitboxScript _bottomHitbox;
@@ -17,10 +19,15 @@ public class PushDetection : MonoBehaviour
 
     public bool IsBeingPushed { get; set; }
 
+    public bool CanBePushed { get { return PushPhase == 0; }  }
+
+    public int PushPhase { get; set; } = 0;
+
     public float _pushedTime = 0f;
 
     private void Start()
     {
+        _director = GameObject.Find("Director");
         _globalParameters = _director.GetComponent<GlobalParameters>();
     }
 
@@ -36,25 +43,46 @@ public class PushDetection : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (!IsBeingPushed)
+        if (!IsBeingPushed || !CanBePushed)
         {
             _pushedTime = 0f;
             return;
         }
-        
+
         _pushedTime += Time.deltaTime;
         if (_pushedTime > _globalParameters._requiredPushingTime) Push();
     }
 
     internal void Push()
     {
-        var dir = CanBePushedInThisDirection();
-        if (!dir.HasValue)
+        IsBeingPushed = false;
+        var maybeDir = CanBePushedInThisDirection();
+        if (!maybeDir.HasValue)
         {
             Debug.Log($"ERROR: We try to push {name}, but no direction is available");
             return;
         }
+        var dir = maybeDir.Value;
 
         // Code voor het duwen.
+        PushOn(dir);
+    }
+    
+    internal void PushOn(Direction dir)
+    {
+        var vec = DirVec.GetVector(dir);
+        
+        transform.position = transform.position + new Vector3(vec.x * _globalParameters._rollSpeed / 4, vec.y * _globalParameters._rollSpeed / 4, 0f);
+        print(PushPhase);
+        PushPhase = (PushPhase + 1) % 4;
+        if (PushPhase == 0) return;
+        StartCoroutine(WaitThenPushOn(dir));
+    }
+
+
+    IEnumerator WaitThenPushOn(Direction dir)
+    {
+        yield return new WaitForSeconds(_globalParameters._rollDuration / 4);
+        PushOn(dir);
     }
 }
