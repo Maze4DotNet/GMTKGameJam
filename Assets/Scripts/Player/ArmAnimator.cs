3939cf09920b685cdf1d71a48ccd40460f62a3c2
Kirbman10 <38724165+Kirbman10@users.noreply.github.com>
2022-07-16 15:12:19 +0200

    dingen zijn goed! :)_

--- Assets/Scripts/Player/ArmAnimator.cs
+++ Assets/Scripts/Player/ArmAnimator.cs
@@ -5,31 +5,37 @@ using UnityEngine;
 public class ArmAnimator : MonoBehaviour
 {
     public bool _renderArms;
-    private Animator _parentAnim;
-    private Animator _ownAnim;
+    private Animator _anim;
     private SpriteRenderer _spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        _parentAnim = transform.parent.GetComponent<Animator>();
-        _ownAnim = gameObject.GetComponent<Animator>();
+        _anim = gameObject.GetComponent<Animator>();
         _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (_renderArms)
+    public void RenderArms(Direction dir){
+        _spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+        switch (dir)
         {
-            _spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
-            _ownAnim.SetBool("up", _parentAnim.GetBool("up"));
-            _ownAnim.SetBool("down", _parentAnim.GetBool("down"));
-            _ownAnim.SetBool("left", _parentAnim.GetBool("left"));
-            _ownAnim.SetBool("right", _parentAnim.GetBool("right"));
+            case Direction.Up:
+                _anim.SetBool("up", true);
+                break;
+            case Direction.Down:
+                _anim.SetBool("down", true);
+                break;
+            case Direction.Left:
+                _anim.SetBool("left", true);
+                break;
+            case Direction.Right:
+                _anim.SetBool("right", true);
+                break;
         }
-        else
-        {
-            _spriteRenderer.color = new Color(1f, 1f, 1f, 0f);
-        }
+    }
+    public void UnrenderArms(){
+        _spriteRenderer.color = new Color(1f, 1f, 1f, 0f);
+        _anim.SetBool("up", false);
+        _anim.SetBool("down", false);
+        _anim.SetBool("left", false);
+        _anim.SetBool("right", false);
     }
 }
